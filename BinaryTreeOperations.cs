7f8f4473cfd3d753dfd0c6bc8ae1e8e1a86ca5f3
vivek singh <vivek.vns.056@gmail.com>
2020-05-23 15:05:20 -0400

    Added Min/Max operation for Binary Tree

--- BinaryTreeOperations.cs
+++ BinaryTreeOperations.cs
@@ -1,10 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace DataStructureImplementationCSharp
 {
-    class BinaryTreeOperations
+   public  class BinaryTreeOperations
     {
+      public  BSTNode root = null;
+
+        public  static BSTNode AddNode(BSTNode root, int n)
+        {
+            BSTNode newNode = new BSTNode(n);
+            if(root == null)
+            {
+                root = newNode;
+                return root;
+            }
+            else if(n <= root.data)
+            { 
+             root.leftNode= AddNode(root.leftNode, n);
+            }
+            else
+            {
+              root.rightNode =  AddNode(root.rightNode, n);
+            }
+            return root;
+        }
+
+        public  int Min(BSTNode root)
+        {
+            if(root == null)
+            {
+                return -1;
+            }
+            while(root.leftNode != null)
+                {
+                root = root.leftNode;
+             
+                }
+            return root.data;
+        }
+        public int Max(BSTNode root)
+        {
+            if (root == null)
+            {
+                return -1;
+            }
+            while (root.rightNode != null)
+            {
+                root = root.rightNode;
+
+            }
+            return root.data;
+        }
+    }
+
+    public class BSTNode
+    {
+      public  int data;
+      public  BSTNode leftNode;
+      public  BSTNode rightNode;
+
+        public BSTNode(int data)
+        {
+            this.data = data;
+            this.leftNode = null;
+            this.rightNode = null;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,16 @@ namespace DataStructureImplementationCSharp
         {
             Console.WriteLine("Hello World!");
 
-            Console.WriteLine("Implement all DS in C#");
+            Console.WriteLine("Add node in Binary Search Tree.");
+            BSTNode root = null;
+            root =BinaryTreeOperations.AddNode(root, 10);
+            root =BinaryTreeOperations.AddNode(root, 5);
+            root =BinaryTreeOperations.AddNode(root, 15);
+            root = BinaryTreeOperations.AddNode(root, 12);
+            root = BinaryTreeOperations.AddNode(root, 25);
+            BinaryTreeOperations ops = new BinaryTreeOperations();            
+            Console.WriteLine("Minimum Value is Tree is : " + ops.Min(root));
+            Console.WriteLine("Maximum Value is Tree is : " + ops.Max(root));
         }
     }
 }
